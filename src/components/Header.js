8e41868480a5518cfc13c334da2e4d131317ef79
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-05 17:54:09 +0600

    header and houseList Done

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+// import link
+import { Link } from 'react-router-dom';
+// import logo
+import Logo from '../assets/img/logo.svg';
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex  justify-between items-center'>
+        <Link to='/'>
+          <img src={Logo} alt='' />
+        </Link>
+
+
+        <div className='flex mr-auto'>
+          <Link className='ml-12 bg-purple-200 px-3 text-xl rounded-md' to='/'>
+            Rent
+          </Link>
+          <Link className='mx-4 text-xl' to='/'>
+            Buy
+          </Link>
+          <Link className='mx-4 text-xl' to='/'>
+            Sell
+          </Link>
+
+        </div>
+
+
+        <div className='flex items-center gap-6 '>
+          <Link className='hover:text-violet-900 px-3 py-2 rounded-lg border-2  transition' to='/'>
+            Log in
+          </Link>
+          <Link
+            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
+            to='/'
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
